d973ebc7ef1704f9ba99ad8d9250d3d13d948646
myoneray <myoneray@gmail.com>
2017-07-05 15:37:30 +0800

    展示数组数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HelloWordComponent } from './hello-word/hello-word.component';
 import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWordComponent,
-    UserItemComponent
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
